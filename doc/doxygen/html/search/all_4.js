8438dc3c22b2633e4c98bff73de9ebc405f8ab20
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2015-03-19 11:07:39 +0700

    doxygen

--- doc/doxygen/html/search/all_4.js
+++ doc/doxygen/html/search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['memori',['Memori',['../df/df7/classMemori.html',1,'Memori'],['../d4/dd4/namespaceMemori.html',1,'Memori']]]
+  ['memori',['Memori',['../df/df7/classMemori.html',1,'Memori'],['../d4/dd4/namespaceMemori.html',1,'Memori']]],
+  ['menghitung',['Menghitung',['../df/d37/namespaceMenghitung.html',1,'']]]
 ];
--- doc/doxygen/html/search/namespaces_0.js
+++ doc/doxygen/html/search/namespaces_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['memori',['Memori',['../d4/dd4/namespaceMemori.html',1,'']]]
+  ['memori',['Memori',['../d4/dd4/namespaceMemori.html',1,'']]],
+  ['menghitung',['Menghitung',['../df/d37/namespaceMenghitung.html',1,'']]]
 ];
